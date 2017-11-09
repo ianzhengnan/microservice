7c026d4fa80ab21a42f9ec5642fa3e83c2d43029
Ian Zheng <flks_2005@qq.com>
2017-11-09 18:03:17 +0800

    update1

---
+++ microservice-provider-user-my-metadata/src/main/java/com/ian/dao/UserRepository.java
@@ -0,0 +1,8 @@
+package com.ian.dao;
+
+import com.ian.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
