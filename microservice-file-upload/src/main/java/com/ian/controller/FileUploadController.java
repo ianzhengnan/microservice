73c50b124b5dbdb35e12439d2df767d7a76b0543
Ian Zheng <flks_2005@qq.com>
2017-11-13 13:13:09 +0800

    update8-2

---
+++ microservice-file-upload/src/main/java/com/ian/controller/FileUploadController.java
@@ -0,0 +1,25 @@
+package com.ian.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@Controller
+public class FileUploadController {
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public @ResponseBody String handleFileUpload(@RequestParam(value = "file") MultipartFile file)
+        throws IOException{
+        byte[] bytes = file.getBytes();
+        File fileToSave = new File(file.getOriginalFilename());
+        FileCopyUtils.copy(bytes, fileToSave);
+        return fileToSave.getAbsolutePath();
+    }
+}
