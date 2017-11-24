c6c4e36b27bd883a73f80e66d862f3599cd61c92
Ian Zheng <flks_2005@qq.com>
2017-11-24 14:00:39 +0800

    update spring-cloud

--- spring-cloud/some/src/main/java/com/ian/some/SomeApplication.java
+++ spring-cloud/some/src/main/java/com/ian/some/SomeApplication.java
@@ -4,16 +4,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
-public class ConfigApplication {
+public class SomeApplication {
 
-	@Value("@{my.message}")
+	@Value("${my.message}")
 	private String message;
 
 	@RequestMapping(value = "/getsome")
@@ -22,6 +21,6 @@ public class ConfigApplication {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigApplication.class, args);
+		SpringApplication.run(SomeApplication.class, args);
 	}
 }
--- spring-cloud/ui/src/main/java/com/ian/ui/UiApplication.java
+++ spring-cloud/ui/src/main/java/com/ian/ui/UiApplication.java
@@ -3,9 +3,12 @@ package com.ian.ui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -13,6 +16,12 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableCircuitBreaker
 @EnableZuulProxy
 public class UiApplication {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UiApplication.class, args);
